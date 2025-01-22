6a7cfe3b787fba5636f360e7378cb2821470bc2e
eliaslm <elias.loonamy@gmail.com>
2025-01-22 17:10:28 +0100

    ✨ Add proper supabase login/logout

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,10 +1,6 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { Card } from "@nextui-org/react";
 import { ScrollShadow } from "@nextui-org/react";
-import { Auth } from '@supabase/auth-ui-react'
-import { ThemeSupa } from '@supabase/auth-ui-shared'
-
-import { createClient } from '@supabase/supabase-js'
 
 import DefaultLayout from "@/layouts/default";
 import { MiniAvgBarChart } from "../components/MiniAvgBarChart";
@@ -12,7 +8,7 @@ import { MiniAvgBarChart } from "../components/MiniAvgBarChart";
 import UserCard from "@/components/UserCard";
 import users from '@/../users.json';
 
-const supabase = createClient('http://localhost:8000', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyAgCiAgICAicm9sZSI6ICJhbm9uIiwKICAgICJpc3MiOiAic3VwYWJhc2UtZGVtbyIsCiAgICAiaWF0IjogMTY0MTc2OTIwMCwKICAgICJleHAiOiAxNzk5NTM1NjAwCn0.dc_X5iR_VP_qT0zsiyj_I_OZ2T9FtRU2BBNWN8Bu4GE')
+
 
 function UserCards({ users }) {
   return (
@@ -25,30 +21,15 @@ function UserCards({ users }) {
 }
 
 
-export default function IndexPage() {
-  const [session, setSession] = useState(null)
-  useEffect(() => {
-    supabase.auth.getSession().then(({ data: { session } }) => {
-      setSession(session)
-    })
-
-    const {
-      data: { subscription },
-    } = supabase.auth.onAuthStateChange((_event, session) => {
-      setSession(session)
-    })
-
-    return () => subscription.unsubscribe()
-  }, [])
-
-
+export default function IndexPage({supabaseClient, supabaseSession}) {
+  
   const defaultUsers = users.users;
   const [allUsers, setAllUsers] = useState(defaultUsers);
 
   return (
-    <DefaultLayout>
+    <DefaultLayout supabaseClient={supabaseClient} supabaseSession={supabaseSession}>
       <div className="flex flex-col items-center gap-12">
-        <div className="flex flex-row justify-center gap-12 w-full">
+        <div className="flex flex-row justify-between gap-12 w-full">
           <div className="flex flex-col items-center gap-4 w-1/2">
             <h1 className="text-3xl font-serif">Current Players</h1>
             <div className="w-3/4">
@@ -70,13 +51,6 @@ export default function IndexPage() {
         </div>
         <div className="flex flex-col items-center gap-12">
           <h1 className="text-3xl font-serif">Enter your score and see how you compare!</h1>
-          <div className="flex flex-row gap-12 justify-center w-full">
-            {!session ?
-              <Auth supabaseClient={supabase} appearance={{ theme: ThemeSupa }} />
-              :
-              <p onClick={() => supabase.auth.signOut()}>Logged in!</p>
-            }
-          </div>
           <div className="h-10" />
         </div>
       </div>
