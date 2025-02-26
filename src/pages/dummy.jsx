d98cf18d43fddf05eef5334988c3dda1ba84a732
eliaslm <elias.loonamy@gmail.com>
2025-02-26 16:06:18 +0100

    🚧 Add template for line chart from ShadCN

--- src/pages/dummy.jsx
+++ src/pages/dummy.jsx
@@ -3,6 +3,105 @@ import AvgTable from '../components/AvgTable';
 import { useState, useEffect } from "react";
 import users from '@/../users.json';
 import CurrentPlayers from "../components/CurrentPlayers";
+import { CartesianGrid, Line, LineChart, XAxis } from "recharts"
+
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import {
+  ChartContainer,
+  ChartTooltip,
+  ChartTooltipContent,
+} from "@/components/ui/chart"
+
+const chartData = [
+  { month: "January", desktop: 186, mobile: 80 },
+  { month: "February", desktop: 305, mobile: 200 },
+  { month: "March", desktop: 237, mobile: 120 },
+  { month: "April", desktop: 73, mobile: 190 },
+  { month: "May", desktop: 209, mobile: 130 },
+  { month: "June", desktop: 214, mobile: 140 },
+]
+
+const chartConfig = {
+  desktop: {
+    label: "Desktop",
+    color: "hsl(var(--chart-1))",
+  },
+  mobile: {
+    label: "Mobile",
+    color: "hsl(var(--chart-2))",
+  },
+}
+
+export function AvgLines() {
+  return (
+    <div className="flex flex-col items-center gap-4">
+      <h1 className="text-2xl font-serif">
+        Development
+      </h1>
+      <Card>
+        <CardHeader>
+          <CardTitle>Scores by Date</CardTitle>
+          <CardDescription>January - June 2024</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <ChartContainer config={chartConfig}>
+            <LineChart
+              accessibilityLayer
+              data={chartData}
+              margin={{
+                left: 12,
+                right: 12,
+              }}
+            >
+              <CartesianGrid vertical={false} />
+              <XAxis
+                dataKey="month"
+                tickLine={false}
+                axisLine={false}
+                tickMargin={8}
+                tickFormatter={(value) => value.slice(0, 3)}
+              />
+              <ChartTooltip cursor={false} content={<ChartTooltipContent />} />
+              <Line
+                dataKey="desktop"
+                type="monotone"
+                stroke="var(--color-desktop)"
+                strokeWidth={2}
+                dot={false}
+              />
+              <Line
+                dataKey="mobile"
+                type="monotone"
+                stroke="var(--color-mobile)"
+                strokeWidth={2}
+                dot={false}
+              />
+            </LineChart>
+          </ChartContainer>
+        </CardContent>
+        <CardFooter>
+          <div className="flex w-full items-start gap-2 text-sm">
+            <div className="grid gap-2">
+              <div className="flex items-center gap-2 font-medium leading-none">
+                Trending up by 5.2% this month
+              </div>
+              <div className="flex items-center gap-2 leading-none text-muted-foreground">
+                Showing total visitors for the last 6 months
+              </div>
+            </div>
+          </div>
+        </CardFooter>
+      </Card>
+    </div>
+  )
+}
 
 
 const testUsers = users;
@@ -30,6 +129,7 @@ export default function DummyPage({ supabaseClient, supabaseSession }) {
 
     setUsers(users);
     setUserData(userData);
+    console.log(userData);
 
     const userMap = new Map();
     users.forEach(user => {
@@ -63,11 +163,15 @@ export default function DummyPage({ supabaseClient, supabaseSession }) {
   return (
     <DefaultLayout supabaseClient={supabaseClient} supabaseSession={supabaseSession}>
       <div className='w-full flex flex-col items-center gap-10'>
-        <AvgTable users={userAverages} isLoading={loading} />
+
+        <div className="flex items-start justify-center gap-20 w-full">
+          <AvgTable users={userAverages} isLoading={loading} />
+          <AvgLines />
+        </div>
 
         <div className="flex flex-col items-center gap-4">
           <h1 className="text-2xl font-serif">Current players</h1>
-          <CurrentPlayers users={testUsers.users}/>
+          <CurrentPlayers users={testUsers.users} />
         </div>
       </div>
     </DefaultLayout>
