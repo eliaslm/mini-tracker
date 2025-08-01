0099bee5b36626a6d9d7bc643e4adeb6d3db5415
eliaslm <elias.loonamy@gmail.com>
2025-08-01 13:49:21 +0200

    🔐 Move connection vars to env file

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,9 @@ import { createClient } from '@supabase/supabase-js';
 import './App.css'
 import IndexPage from "@/pages/index";
 
-const supabaseClient = createClient('http://localhost:8000', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyAgCiAgICAicm9sZSI6ICJhbm9uIiwKICAgICJpc3MiOiAic3VwYWJhc2UtZGVtbyIsCiAgICAiaWF0IjogMTY0MTc2OTIwMCwKICAgICJleHAiOiAxNzk5NTM1NjAwCn0.dc_X5iR_VP_qT0zsiyj_I_OZ2T9FtRU2BBNWN8Bu4GE')
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+const supabaseClient = createClient(supabaseUrl, supabaseKey)
 
 function App() {
   const [supabaseSession, setSupabaseSession] = useState(null)
