f80498a465becc4277bc7b1967d37ca6b069d623
eliaslm <elias.loonamy@gmail.com>
2025-08-01 15:40:25 +0200

    🔧 Load base url from env file in vite config

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,21 @@
 import path from "path"
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: '/mini-tracker/',
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+export default defineConfig(({ command, mode }) => {
+  // Load environment variables based on the current mode (development, production, etc.)
+  // 'process.cwd()' ensures that the .env files are loaded from the root of your project
+  const env = loadEnv(mode, process.cwd(), ''); // The third argument '' loads all env vars
+
+  return {
+    plugins: [react()],
+    // Use the environment variable for the base URL
+    // Provide a fallback value (e.g., '/') in case the env var is not set
+    base: env.VITE_APP_BASE_URL || '/mini-tracker/',
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
     },
-  },
+  };
 })
